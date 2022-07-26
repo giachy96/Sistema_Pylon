665fa1dd7f0e40b681dd1520bfa8357b949fb3b8
MatteoToms <matomassini@alice.it>
2022-07-26 21:13:02 +0200

    Aggiunta Smistamento dati Rx telecomando

--- Firmware Sistema/Sistema Pylon Rev2 18-07/Semaforo Rev2/RX_SEMAFORO_Rev1/RX_SEMAFORO_Rev1.ino
+++ Firmware Sistema/Sistema Pylon Rev2 18-07/Semaforo Rev2/RX_SEMAFORO_Rev1/RX_SEMAFORO_Rev1.ino
@@ -6,8 +6,8 @@ LoRa_E22 e22ttl(2, 3); // Arduino RX --> e22 TX - Arduino TX --> e22 RX
 #define SOFTRX 10 // MT - define rx pin 10
 #define SOFTTX 11 // MT - define tx pin 11
 SoftwareSerial SwSerial(SOFTRX,SOFTTX);
-String State;
-String OldState;
+String State="000";
+String TxData="0";
 
 void setup() {
      pinMode(SOFTTX, OUTPUT); // MT - Softrx - pin mapping output
@@ -23,20 +23,23 @@ void setup() {
  
 }
  
-void loop() {
- 
-    // If something available
-  if (e22ttl.available()>1) {
-      // read the String message
-    ResponseContainer rc = e22ttl.receiveMessage();
-    // Is something goes wrong print error
-    if (rc.status.code!=1){
-        rc.status.getResponseDescription();
-    }else{
-       if (SwSerial.available()){
-    State=SwSerial.read();
-      }
-    SwSerial.println(rc.data);
+void loop() { 
+  if (e22ttl.available()>1) {  // If there is something arrived from lora (Transmitter)
+    ResponseContainer rc = e22ttl.receiveMessage();// Receive message
+    if (rc.status.code!=1){ // If there is some problem
+      rc.status.getResponseDescription(); //Get report
     }
+    else{ //If there isn't any problem we're going to receive press 
+      TxData=rc.data; //Assign incoming data on TxData variable
+    }
+  if (TxData !="0" && (State=="200" && State=="300")){ //Condition for sent to mega press
+    if (SwSerial.available()){
+      State=SwSerial.read();
+      }
+      if (State=="200" && State=="300"){ //If nothing has change in the previous condition
+    SwSerial.println(TxData); //Send Txdata from rx to Mega
+    }
+    TxData="0"; //Reset condition for set data
   }
+ }
  }
