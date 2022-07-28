6dd0cb53a7d4d0b6e340b3faddbb804101b690d1
giachy96 <frfgia@libero.it>
2022-07-28 22:43:34 +0200

    OK

--- Firmware Sistema/Sistema Pylon Rev2 18-07/FINTOTABELLONE/NanoCentrale/nano_lcd/nano_lcd.ino
+++ Firmware Sistema/Sistema Pylon Rev2 18-07/FINTOTABELLONE/NanoCentrale/nano_lcd/nano_lcd.ino
@@ -1,34 +1,45 @@
-#include"SoftwareSerial.h"
+#include "SoftwareSerial.h"
 #include <SPI.h>
 #include <DMD2.h>
 #include <fonts/SystemFont5x7.h>
 #include <fonts/Arial14.h>
 
-
 // You can change to a smaller font (two lines) by commenting this line,
 // and uncommenting the line after it:
-//const uint8_t *FONT = Arial14;
+// const uint8_t *FONT = Arial14;
 const uint8_t *FONT = SystemFont5x7;
 
+SoftwareSerial Serial1(4, 5); // RX, TX
 
+int code;
 
 #define DISPLAYS_ACROSS 2
 #define DISPLAYS_DOWN 3
 
-SoftDMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);  // DMD controls the entire display
-DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
+SoftDMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN); // DMD controls the entire display
+DMD_TextBox box(dmd);                        // "box" provides a text box to automatically write to/scroll the display
 
 // the setup routine runs once when you press reset:
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+  Serial1.begin(9600);
   dmd.setBrightness(255);
   dmd.selectFont(FONT);
   dmd.begin();
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
-  dmd.drawString(1, 5, "Paolo");
-  dmd.drawString(1, 22, "Alessandro");
-  dmd.drawString(1, 40, "Daniele");
+void loop()
+{
+  if (Serial1.available())
+  {
+    code = Serial1.read();
+    if (code == 500)
+    {
+      dmd.drawString(1, 5, "STOP");
+      dmd.drawString(1, 19, "STOP");
+      dmd.drawString(1, 38, "STOP");
+    }
+  }
 }
