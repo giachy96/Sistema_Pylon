4e465596f164acc08a0970c56291816e19982dc5
giachy96 <frfgia@libero.it>
2022-08-01 09:54:19 +0200

    ok

---
+++ Firmware Sistema/Sistema Pylon Rev2 18-07/TABELLONE/nano_palo1/nano_palo1.ino
@@ -0,0 +1,50 @@
+#include <AltSoftSerial.h>
+
+AltSoftSerial altSerial;
+
+
+String msg;
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  delay(200);
+  altSerial.begin(9600);
+
+}
+
+
+void loop() {
+
+
+if (altSerial.available()) {
+    msg = altSerial.read();
+    Serial.println(msg);
+  }
+
+ altSerial.println("SONO IL PALO1");
+ delay(1000);
+
+
+
+
+}
+
+
+
+void readMasterPort() {
+   
+  while (ArduinoMaster.available()) {
+
+    delay(10);
+    if (ArduinoMaster.available() > 0) {
+         
+      char c = ArduinoMaster.read();  //gets one byte from serial buffer
+      msg += c; //makes the string readString
+    }
+  }
+  ArduinoMaster.flush();
+
+}
