c9d6dac089835b4c37ca33a5cd91b60d156dea4d
MatteoToms <109790965+MatteoToms@users.noreply.github.com>
2022-08-08 13:28:10 +0200

    Aggiornamento condizioni e commento

--- Firmware Sistema/Sistema Pylon Rev2 18-07/Palo 2e3/TXROSSO_Palo2_U8x8/TXROSSO_Palo2_U8x8.ino
+++ Firmware Sistema/Sistema Pylon Rev2 18-07/Palo 2e3/TXROSSO_Palo2_U8x8/TXROSSO_Palo2_U8x8.ino
@@ -34,6 +34,7 @@ unsigned long Delaypress=2500;
 unsigned long Delaysend=200;
 unsigned long Freq=500;
 unsigned long Phase=0;
+unsigned long WindowTransm=150;
 unsigned long Sync=0;
 int Transm=0;
 int Press=0;
@@ -61,18 +62,22 @@ void setup() {
 }
 
 void loop() {
-
-if (State==Show || State==Race){
-  if (Sync<millis()){
+currentMillis=millis();
+if (State==Show || State==Race){ // Ogni volta che ricevo un nuovo stato memorizzo un millis che mi serve come sincronismo (Sync) , elaboro la conferma
+  if (Sync<currentMillis){ // se ancora non è arrivato il momento di trasmettere assegno il flag Transm a zero
   Transm=0;
   }
-  else if (Sync>=millis()){
-    Transm=1;
-    while (Sync>millis) {
+  else if (Sync>=currentMillis && (currentMillis-Sync)<= WindowTransm){ // Se è arrivato il momento di trasmettere, ovvero ho appena superato il tempo Sync e sono nella finestra temporale da Sync a Sync+150ms
+    Transm=1; // Inserisco il flag di trasmissione
+    Sync=Sync+Freq; // incremento il valore di Sync aggiungendo la frequenza
+    }
+  }
+  else if (Sync>=currentMillis && (currentMillis-Sync)> WindowTransm){ // Se è arrivato il momento di trasmettere ma non siamo più nella finestra temporale di sicurezza ovvero Sync/Sync+150
+    Transm=0; // non permettere di trasmettere
+    while (Sync>currentMillis) { //Incrementa il valore di Sync con la frequenza finchè Sync non è di nuovo maggiore di currentmillis (sicurezza forse inutile se il codice impunta previene la trasmissione sovrapposta)
     Sync=Sync+Freq;
     }
   }
-}
 
  currentMillis = millis();
 
@@ -89,68 +94,69 @@ if (State==Show || State==Race){
       digitalWrite(buzzer , HIGH);
     }
     currentMillis = millis();
-    if ((((currentMillis - previousMillis)>=interval) || changeState==1) && (State !=Show || State != Race || State != End)) {
+    if ((((currentMillis - previousMillis)>=interval) || changeState==1) && (State !=Show || State != Race || State != End)) { // Casistica per display, se non siamo in Show, Race o End, metti la schermata 0 e aggiorna ogni 5 sec, o aggiorna quando il flag changestate  è attivo
       previousMillis = currentMillis;
       Serial.println("Case0");
       draw(0, u8x8);
       changeState=0;
     }
-if ((((currentMillis - previousMillis)>=interval) || changeState==1) && State==Show) {
+if ((((currentMillis - previousMillis)>=interval) || changeState==1) && State==Show) { // Casistica per display, se siamo in Show metti la schermata 1 e aggiorna ogni 5 sec, o aggiorna quando il flag changestate è attivo
       previousMillis = currentMillis;
       Serial.println("Case1");
       draw(1, u8x8);
       changeState=0;
     }
-    if (changeState==1 && State==Race) {
+    if (changeState==1 && State==Race) { // Casistica per display, se siamo in Race metti la schermata 2 e aggiorna solo quando il flag changestate è attivo
       previousMillis = currentMillis;
       Serial.println("Case2");
       draw(2, u8x8);
       changeState=0;
     }
-    if ((((currentMillis - previousMillis)>=interval) || changeState==1) && State==End) {
+    if ((((currentMillis - previousMillis)>=interval) || changeState==1) && State==End) { // Casistica per display, se siamo in End metti la schermata 6 e aggiorna ogni 5 sec, o aggiorna quando il flag changestate è attivo
+      previousMillis = currentMillis;
       previousMillis = currentMillis;
       draw(6, u8x8);
       Serial.println("Case6");
       changeState=0;
     }
-    if (e22ttl.available()>1){
+    if (e22ttl.available()>1){ 
       Serial.println("ricevo qualcosa");
       ResponseContainer rc = e22ttl.receiveMessage();// Receive message
       if (rc.status.code!=1){ // If there is some problem
         rc.status.getResponseDescription(); //Get report
         }
       else { //If there isn't any problem we're going to receive press 
-        State=rc.data; //Assign incoming data on TxData variable
+        State=rc.data; //Assign incoming data on State variable
         Serial.println("Nuovo stato");
-        Serial.print(State);
-        changeState=1;
-      Sync=(millis()+Phase+5*Freq);
+        Serial.print(State); // ricevo il nuovo stato
+        changeState=1; //attivo il flag changeState=1, lo uso sopra per il primo aggiornamento display 
+      Sync=(millis()+Phase+2*Freq); //prendo il tempo per la sincronia di invio, millis(tempo corrente) inserisco la fase (0 per il primo, Freq/2 per per il secondo) e mi metto in sicurezza aggiungendo due volte la frequenza
       }
     } 
     CurrentPress=millis();
-      if (digitalRead(taglio) == LOW && (CurrentPress-Lastpress)>=Delaypress && State==Show) {
-      Lastpress=millis();
-      tone(buzzer, 1000, 200);
-      Press=1;
+      if (digitalRead(taglio) == LOW && (CurrentPress-Lastpress)>=Delaypress && State==Show) { // Se sono in show, è passato il tempo di sicurezza e il bottone è premuto
+      Lastpress=millis(); // memorizzo il tempo della pressione
+      tone(buzzer, 1000, 200); //invio il tono
+      Press=1; //metto il flag su Press
     }  
       CurrentPress=millis();
-      if (digitalRead(taglio) == LOW && (CurrentPress-Lastpress)>=Delaypress && State==Race) {
-      Lastpress=millis();
-      tone(buzzer, 1000, 200);
-      Pressr=1;
+      if (digitalRead(taglio) == LOW && (CurrentPress-Lastpress)>=Delaypress && State==Race) { // Se sono in race, è passato il tempo di sicurezza e il bottone è premuto
+      Lastpress=millis();// memorizzo il tempo della pressione
+      tone(buzzer, 1000, 200);//invio il tono
+      Pressr=1;//metto il flag su Pressr
     }  
-    if (Press==1 && Transm==1 && State==Show){
-    ResponseStatus rs = e22ttl.sendFixedMessage(0, 0, 3,"<211a>");
-    Press=0;
+    if (Press==1 && Transm==1 && State==Show){ //se sono in Show, se ho il flag della pressione di show e se ho consenso alla trasmissione con Transm
+    ResponseStatus rs = e22ttl.sendFixedMessage(0, 0, 3,"<211a>"); //mando il messaggio
+    Press=0; //azzero il flag pressione di show
     }
-    else if (Press==1 && State !=Show){
+    else if (Press==1 && State !=Show){ //se ho il flag di pressione ma nel frattempo è cambiato lo stato togli il flag della pressione
       Press=0;
     }
-    if (Pressr==1 && Transm==1 && State==Race){
-    ResponseStatus rs = e22ttl.sendFixedMessage(0, 0, 3,"<311a>");
-    Pressr=0;
+    if (Pressr==1 && Transm==1 && State==Race){//se sono in Race, se ho il flag della pressione di Race e se ho consenso alla trasmissione con Transm
+    ResponseStatus rs = e22ttl.sendFixedMessage(0, 0, 3,"<311a>");//mando il messaggio
+    Pressr=0;//azzero il flag pressione di Race
     }
-    else if (Pressr==1 && State !=Race){
+    else if (Pressr==1 && State !=Race){//se ho il flag di pressione ma nel frattempo è cambiato lo stato togli il flag della pressione
       Pressr=0;
     }
 }
