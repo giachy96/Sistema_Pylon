10aeabc98d1731c88168752520842bf942733ce7
giachy96 <frfgia@libero.it>
2022-08-14 23:41:33 +0200

    rx colori palo 2/3

--- Firmware Sistema/Sistema Pylon Rev2 12-08/TABELLONE/Teensy_/Teensy/Teensy.ino
+++ Firmware Sistema/Sistema Pylon Rev2 12-08/TABELLONE/Teensy_/Teensy/Teensy.ino
@@ -56,15 +56,15 @@ void loop() {
   recvSerial1();
   /// showSerial1();attenziona al new data
   recvSerial2();
- // showSerial2();
+  // showSerial2();
   recvSerial3();
- // showSerial3();
+  // showSerial3();
   recvSerial4();
- // showSerial4();
+  // showSerial4();
   recvSerial5();
-//  showSerial5();
+  //  showSerial5();
   recvSerial6();
- // showSerial6();
+  // showSerial6();
   recvSerial7();
   //showSerial7();attenziona al new data
   recvSerial8();
@@ -78,6 +78,8 @@ void loop() {
       Serial7.print("<6000>");
       Serial3.print("<6000>");
       Serial5.print("<6000>");
+      Serial4.print("<6000>");
+      Serial2.print("<6000>");
       oldMillis = millis();
     }
     if (digitalRead(pGo) == LOW ) {
@@ -87,6 +89,8 @@ void loop() {
       Serial7.print("<3000>");
       Serial3.print("<3000>");
       Serial5.print("<3000>");
+      Serial4.print("<3000>");
+      Serial2.print("<3000>");
       oldMillis = millis();
     }
     if (digitalRead(pShow) == LOW ) {
@@ -96,6 +100,8 @@ void loop() {
       Serial7.print("<2000>");
       Serial3.print("<2000>");
       Serial5.print("<2000>");
+      Serial4.print("<2000>");
+      Serial2.print("<2000>");      
       oldMillis = millis();
     }
   }
@@ -113,7 +119,7 @@ void loop() {
         decodecomma(Rxs8, values);
         tempirosso[values[1].toInt()] = values[2];
       }
-       if (Rxs8.indexOf("5514") != -1 ) {
+      if (Rxs8.indexOf("5514") != -1 ) {
         decodecomma(Rxs8, values);
         tempirosso[11] = values[2];
         Serial3.print("<5514>");
@@ -133,14 +139,14 @@ void loop() {
       Serial6.print(pack);
       Serial.println(pack);
 
-       if (Rxs1.indexOf("4534") != -1 ) {
+      if (Rxs1.indexOf("4534") != -1 ) {
         decodecomma(Rxs1, values);
         tempiblu[values[1].toInt()] = values[2];
       }
-       if (Rxs1.indexOf("5534") != -1 ) {
+      if (Rxs1.indexOf("5534") != -1 ) {
         decodecomma(Rxs1, values);
         tempiblu[11] = values[2];
-         Serial3.print("<5534>");
+        Serial3.print("<5534>");
       }
 
     }
@@ -156,21 +162,21 @@ void loop() {
       pack.concat(">");
       Serial6.print(pack);
 
-       if (Rxs7.indexOf("4524") != -1 ) {
+      if (Rxs7.indexOf("4524") != -1 ) {
         decodecomma(Rxs7, values);
         tempiverde[values[1].toInt()] = values[2];
       }
-       if (Rxs7.indexOf("5524") != -1 ) {
+      if (Rxs7.indexOf("5524") != -1 ) {
         decodecomma(Rxs7, values);
         tempiverde[11] = values[2];
-         Serial3.print("<5524>");
+        Serial3.print("<5524>");
       }
 
     }
     newData7 = false;
   }
 
-    if (newData6 == true) {
+  if (newData6 == true) {
     String Rxs6;
     Rxs6 = receivedChars6;
     if (Rxs6.indexOf("3513") != -1 ) { // avvio rosso
@@ -179,7 +185,7 @@ void loop() {
       pack.concat(">");
       Serial8.print(pack);
     }
-       if (Rxs6.indexOf("3523") != -1 ) { // avvio verde
+    if (Rxs6.indexOf("3523") != -1 ) { // avvio verde
       String pack = "<";
       pack.concat(receivedChars6);
       pack.concat(">");
@@ -191,12 +197,14 @@ void loop() {
       pack.concat(">");
       Serial1.print(pack);
     }
-     if (Rxs6.indexOf("3001") != -1 ) { // avvio tutti i tagli
+    if (Rxs6.indexOf("3001") != -1 ) { // avvio tutti i tagli
       String pack = "<";
       pack.concat(receivedChars6);
       pack.concat(">");
       Serial3.print(pack); // palo1
       Serial5.print(pack);//palo2-3 rosso
+      Serial4.print(pack);//palo2-3 rosso
+      Serial2.print(pack);//palo2-3 rosso
     }
     newData6 = false;
   }
