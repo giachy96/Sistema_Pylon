bba43a476a919fce955a845c53ae6242c94a2ff5
giachy96 <frfgia@libero.it>
2022-08-22 17:29:35 +0200

    ok

--- Firmware Sistema/Sistema Pylon Rev2 12-08/Centrale lettura nomi/Centrale_Rev2/Centrale_Rev2.ino
+++ Firmware Sistema/Sistema Pylon Rev2 12-08/Centrale lettura nomi/Centrale_Rev2/Centrale_Rev2.ino
@@ -15,20 +15,23 @@ byte Add = 0;
 byte Chan = 5;
 
 
-int buzzer = 12 ;
+int buzzer = 12;
 int pstop = 2;
 int pgo = 3;
 int pshow = 6;
 int r_print = 8;
 int ledstatusPC = 4;
-
+bool statusPC = false;
 int checkPC = 7;
 int pinbatt = A0;
-int secure = 0; // valore per evitare la pressione doppia
-unsigned long previousMillis = 0; // will store last time voltage was updated
+int secure = 0;                    // valore per evitare la pressione doppia
+unsigned long previousMillis = 0;  // will store last time voltage was updated
+unsigned long oldPress = 0;
 long intervalcheckPC = 1000;
-long oldcheckPC = 0 ;
+long oldcheckPC = 0;
 long interval = 3000;  // refresh schermo
+long intervalPress = 1000;  // refresh schermo
+int doublePress = 0;
 String arraytempirosso[11];
 String arraytaglirosso[10];
 String arraytempiverde[11];
@@ -40,21 +43,21 @@ String RxData = "";
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 // lora parametri
-LoRa_E22 e22ttl(10, 11); // Arduino RX --> e22 TX - Arduino TX --> e22 RX
+LoRa_E22 e22ttl(10, 11);  // Arduino RX --> e22 TX - Arduino TX --> e22 RX
 
 
 
 void setup() {
-  Serial.begin(9600);        // imposta la comunicazione seriale
-  Serial2.begin(9600);  // Initialize HwSerial per la Stampanate
-  printer.begin();        // Init printer (same regardless of serial type)
-  pinMode(buzzer, OUTPUT);        // imposta come input il pin 10
-  pinMode(pstop, INPUT_PULLUP);        // imposta come input il pin 11
-  pinMode(pgo, INPUT_PULLUP );        // imposta come input il pin 12
-  pinMode (pinbatt , INPUT);
-  pinMode( r_print , INPUT_PULLUP );       // imposta come input il pin 12
-  pinMode(checkPC , INPUT );
-  pinMode(pshow , INPUT_PULLUP );
+  Serial.begin(9600);            // imposta la comunicazione seriale
+  Serial2.begin(9600);           // Initialize HwSerial per la Stampanate
+  printer.begin();               // Init printer (same regardless of serial type)
+  pinMode(buzzer, OUTPUT);       // imposta come input il pin 10
+  pinMode(pstop, INPUT_PULLUP);  // imposta come input il pin 11
+  pinMode(pgo, INPUT_PULLUP);    // imposta come input il pin 12
+  pinMode(pinbatt, INPUT);
+  pinMode(r_print, INPUT_PULLUP);  // imposta come input il pin 12
+  pinMode(checkPC, INPUT);
+  pinMode(pshow, INPUT_PULLUP);
   pinMode(ledstatusPC, OUTPUT);
   e22ttl.begin();
   lcd.init();
@@ -63,78 +66,99 @@ void setup() {
   digitalWrite(buzzer, HIGH);
   delay(500);
   digitalWrite(buzzer, LOW);
-
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - oldcheckPC >= intervalcheckPC ) {
+  if (currentMillis - oldcheckPC >= intervalcheckPC) {
     if (digitalRead(checkPC) == HIGH) {
       digitalWrite(ledstatusPC, HIGH);
-      Serial.println("LED Acceso");
+      statusPC = true;
     } else {
       digitalWrite(ledstatusPC, LOW);
-      Serial.println("LED Spento");
+      statusPC = false;
     }
     oldcheckPC = millis();
   }
 
-  if (tensionebatt( pinbatt ) < 9.5) { // allarme tensione batteria
-    tone( buzzer, 5000, 500);
+  if (currentMillis - oldPress >= intervalPress) {
+    doublePress = 0;
   }
-  if (currentMillis - previousMillis >= interval ) {
+
+  if (tensionebatt(pinbatt) < 9.5) {  // allarme tensione batteria
+    tone(buzzer, 5000, 500);
+  }
+  if (currentMillis - previousMillis >= interval) {
     // save the last time you read
     previousMillis = currentMillis;
     // read the tension of the battery
-    float vout = tensionebatt( pinbatt);
+    float vout = tensionebatt(pinbatt);
     lcd.setCursor(3, 3);
     lcd.print("V.Batt.");
     lcd.print(vout);
   }
 
-  if (digitalRead(pshow) == LOW ) {
-    tone( buzzer, 5000, 200);
+  if (digitalRead(pshow) == LOW && doublePress == 0) {
+    tone(buzzer, 4000, 200);
+    doublePress = 1;
+    oldPress = millis();
     //Serial.println("800,0,0,0,0,0,0,0,0,0");
-    draw(1, lcd);
+    draw(2, lcd);
   }
 
-  if (digitalRead(pgo) == LOW ) {
-    tone( buzzer, 5000, 200);
+  if (digitalRead(pgo) == LOW && doublePress == 0) {
+    tone(buzzer, 4000, 200);
     //Serial.println("800,0,0,0,0,0,0,0,0,0");
-    draw(1, lcd);
-  }
-  if ( digitalRead(pstop) == LOW ) {
-    tone( buzzer, 4000, 200);
-    // stampatotali (gara, manche_rx, round_rx , nome_rosso,  tempi_rosso, nome_verde,  tempi_verde, nome_blu,  tempi_blu) ;
     draw(3, lcd);
-
+    doublePress = 1;
+    oldPress = millis();
+  }
+  if (digitalRead(pstop) == LOW && doublePress == 0) {
+    tone(buzzer, 5000, 200);
+    // stampatotali (gara, manche_rx, round_rx , nome_rosso,  tempi_rosso, nome_verde,  tempi_verde, nome_blu,  tempi_blu) ;
+    draw(5, lcd);
+    doublePress = 1;
+    oldPress = millis();
   }
 
-  if ( digitalRead(r_print) == LOW ) { // ristampa tempi
+  if (digitalRead(r_print) == LOW && doublePress == 0) {  // ristampa tempi
     // stampatotali (gara, manche_rx, round_rx , nome_rosso,  old_rosso, nome_verde,  old_verde, nome_blu,  old_blu) ;
-    tone( buzzer, 3000, 200);
+    tone(buzzer, 3000, 200);
+    doublePress = 1;
+    oldPress = millis();
   }
 
   if (RxData == "2000") {  // se ricevo show
-    draw(1, lcd);
-    tone( buzzer, 3000, 200);
-    // provstampa();
+    draw(2, lcd);
+    tone(buzzer, 3000, 200);
     RxData = "";
   }
-  if (RxData.indexOf("5514") != -1 ) { // se ricevo fine 10 giri rosso
+
+    if (RxData == "3000") {  // se ricevo show
+    draw(3, lcd);
+    tone(buzzer, 3000, 200);
+    RxData = "";
+  }
+
+     if (RxData == "6000") {  // se ricevo show
+    draw(5, lcd);
+    tone(buzzer, 5000, 200);
+    RxData = "";
+  }
+
+  if (RxData.indexOf("5514") != -1) {  // se ricevo fine 10 giri rosso
     draw(1, lcd);
     decodestringone(RxData);
-    tone( buzzer, 3000, 200);
-    stampatotali ("Q500" , 2 , 1, "Pippo" , "Pluto"  ,  "Paperino");
-
+    tone(buzzer, 3000, 200);
+    stampatotali("Q500", 2, 1, "Pippo", "Pluto", "Paperino");
     RxData = "";
   }
 
   // 805,Q500,2,3,Matteo,Giacomo,Francesco,Luca,Paolo,Pippo
   // lettura seriale
-  if (digitalRead(checkPC) == HIGH) {
-    if (Serial.available() > 0 ) {
+  if (statusPC == true) {
+    if (Serial.available() > 0) {
       // read the incoming byte:
       incomingByte = Serial.read();
 
@@ -142,7 +166,7 @@ void loop() {
       pos++;
 
 
-      if (incomingByte == 10) {                 //10 is line feed character
+      if (incomingByte == 10) {  //10 is line feed character
         splitCommaSeparated();
 
         //clear the string for next time
