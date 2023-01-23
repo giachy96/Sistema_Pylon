f4e094ebb7610fee4859a67abb506dd1d3208f29
giachy96 <frfgia@libero.it>
2023-01-23 17:06:12 +0100

    Prova message compleated

---
+++ Firmware Sistema/Sistema Pylon Rev2 Message Compleated/Cronometraggio/Rev2/TX_VERDE_CRONORev5/TX_VERDE_CRONORev5.ino
@@ -0,0 +1,279 @@
+/*
+   LoRa E22
+   Write on serial to transfer a message to other device
+   https://www.mischianti.org
+
+   E22        ----- Arduino UNO
+   M0         ----- GND
+   M1         ----- GND
+   TX         ----- PIN 2 (PullUP)
+   RX         ----- PIN 3 (PullUP & Voltage divider)
+   AUX        ----- Not connected
+   VCC        ----- 3.3v/5v
+   GND        ----- GND
+
+*/
+#include "U8x8lib.h"
+#include "Arduino.h"
+#include "LoRa_E22.h"
+#include "lcdcases.h"
+#include "timestructure.h"
+
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE);
+int pulsante = 9;
+int buzzer = 5;
+int pinbatt = A0;
+int bootup = 1;
+int dcase = 0;
+
+LoRa_E22 e22ttl(2, 3);             // Arduino RX --> e22 TX - Arduino TX --> e22 RX
+unsigned long previousMillis = 0;  // will store last time voltage was updated
+unsigned long currentMillis = 0;
+unsigned long CurrentPress = 0;
+unsigned long Lastpress = 0;
+int changeState = 0;
+
+// tempi
+int lapcounter = -1;
+unsigned long tempo_base;
+unsigned long tempo_totale;
+long tempo_parziale[11] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+long* poiparz = &tempo_parziale[0];
+float tempo_flt[11] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int ultimo_tempo;
+int ultimo_inviato = 0;
+float* poitem = &tempo_flt[0];
+
+
+
+// Inizio configurazione Telecomando
+String Race = "3000";
+String Show = "2000";
+String End = "5000";
+String Stop = "6000";
+String Startup = "1000";
+String State = Startup;          //Startup state
+unsigned long interval = 60000;  // constants won't change:
+unsigned long Delaypress = 2000;
+unsigned long DelayStop = 30000;
+String PressShow = "2520";
+String PressTime = "4524";
+String StartRace = "3523";
+String end10lap = "5524";
+String DoubleCut = "4025";
+String StopTime = "6524";
+int Key = 0;
+int Add = 0;
+int Chan = 35;
+//Fine configurazione Telecomando
+
+void setup() {
+  pinMode(pulsante, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  pinMode(pinbatt, INPUT);
+  Serial.begin(9600);
+  delay(500);
+  e22ttl.begin();  // Startup all pins and UART
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  Serial.println("start");  //debug
+  Serial.println(State);    //debug
+  tone(buzzer, 1000, 200);
+}
+
+void loop() {
+
+  currentMillis = millis();
+
+  while (bootup < 2) {
+    //previousMillis = currentMillis;
+    Serial.println("Bootup Case");  //debug
+    draw(0, u8x8);
+    bootup++;
+  }
+
+  while (bootup >= 2) {
+    float voltage = readvoltage(pinbatt);
+    if (voltage < 3.2) {
+      digitalWrite(buzzer, HIGH);
+    }
+    currentMillis = millis();
+
+
+
+    if (((currentMillis - previousMillis) >= interval)  && (State == Startup || State == end10lap || State == Stop || State == DoubleCut ||  State == StopTime )) {
+      previousMillis = currentMillis;
+      Serial.println("Case0");  //debug
+      draw(0, u8x8);
+      changeState = 0;
+    }
+    if (changeState == 1 && State == Show) {  // Casistica per display, se siamo in Show, metti la schermata 1
+      previousMillis = currentMillis; //debug 23-12-2022
+      Serial.println("Case1");  //debug
+      draw(1, u8x8);
+      tempo_base = 0;
+      tempo_totale = 0;
+      memset(tempo_parziale, 0, sizeof(tempo_parziale));
+      memset(tempo_flt, 0, sizeof(tempo_flt));
+      tone(buzzer, 2000, 200);
+      ultimo_tempo = 0;
+      ultimo_inviato = 0;
+      lapcounter = -1;
+      changeState = 0;
+    }
+    if (changeState == 1 && State == StartRace ) {  // Casistica per display, se siamo in StartRace
+      previousMillis = currentMillis; //debug 23-12-2022
+      Serial.println("Case2.1");  //debug
+      tempo_base = millis();
+      lapcounter = lapcounter + 1;
+      Catturatempo(poitem, poiparz, lapcounter, tempo_base);  //balza l'indice a 1Lastpress = millis()
+      float ultimo_tempo = tempo_flt[lapcounter];
+      char bu[10];
+      dtostrf(ultimo_tempo, 4, 3, bu);  //4 is mininum width, 6 is precision
+      String msg = PressTime;
+      msg.concat(",");
+      msg.concat(lapcounter);
+      msg.concat(",");
+      msg.concat(bu);
+      ResponseStatus rs = e22ttl.sendFixedMessage(Key, Add, Chan, msg);
+      tone(buzzer, 2000, 200);
+      draw(3, u8x8);
+      changeState = 0;
+      tone(buzzer, 2000, 200);
+      Lastpress = millis();
+      Serial.println("Dentro lo Start RAce");
+      Serial.println(Lastpress);
+    }
+    // NON SPOSTARE ASSOLUTAMENTE QUESTA PARTE ALTRIMENTI NON FUNZIONA NON SI SA PERCHé
+    CurrentPress = millis();
+    if ((CurrentPress - Lastpress) >= DelayStop && State == StartRace && lapcounter < 10 ) { //se sono passati DelayStop secondi è non ho avuto pressioni
+      Serial.println("Differenza millis");  //debug
+      Serial.println((CurrentPress - Lastpress));  //debug
+      Lastpress = millis();
+      previousMillis = currentMillis;
+      float tot = CalcoloTempo(tempo_flt);
+      tempo_flt[11] = tot;
+      tempo_parziale[11] = (int)(tot * 100);  // per inserire il totale sulla stringa con virgole
+      float ultimo_tempo = tempo_flt[11];
+      char bu[10];
+      dtostrf(ultimo_tempo, 4, 3, bu);  //4 is mininum width, 6 is precision
+      String msg = StopTime;
+      msg.concat(",");
+      msg.concat("11");
+      msg.concat(",");
+      msg.concat(bu);
+      ResponseStatus rs = e22ttl.sendFixedMessage(Key, Add, Chan, msg);
+      draw(8, u8x8);
+      tempo_base = 0;
+      tempo_totale = 0;
+      memset(tempo_parziale, 0, sizeof(tempo_parziale));
+      memset(tempo_flt, 0, sizeof(tempo_flt));
+      ultimo_tempo = 0;
+      ultimo_inviato = 0;
+      lapcounter = -1;
+      changeState = 0;
+      tone(buzzer, 2000, 200);
+      lapcounter = -1;
+      State = StopTime;
+
+      Serial.println("fuori tempo max");  //debug
+
+    }
+    // FINE PARTE DA NON SPOSTARE
+    if (changeState == 1 && State == Race) {  // Casistica per display, se siamo in Race, metti la schermata 2
+      previousMillis = currentMillis; //debug 23-12-2022
+      Serial.println("Case2");  //debug
+      draw(2, u8x8);
+      tempo_base = 0;
+      tempo_totale = 0;
+      memset(tempo_parziale, 0, sizeof(tempo_parziale));
+      memset(tempo_flt, 0, sizeof(tempo_flt));
+      tone(buzzer, 2000, 200);
+      ultimo_tempo = 0;
+      ultimo_inviato = 0;
+      lapcounter = -1;
+      changeState = 0;
+    }
+
+
+    if (changeState == 1 && State == Stop) {  // Casistica per display, se siamo in STOP
+      previousMillis = currentMillis;
+      draw(5, u8x8);
+      tempo_base = 0;
+      tempo_totale = 0;
+      memset(tempo_parziale, 0, sizeof(tempo_parziale));
+      memset(tempo_flt, 0, sizeof(tempo_flt));
+      tone(buzzer, 2000, 200);
+      ultimo_tempo = 0;
+      ultimo_inviato = 0;
+      lapcounter = -1;
+      Serial.println("Case5");  //debug
+      changeState = 0;
+    }
+    if (changeState == 1 && State == DoubleCut) {  // Casistica per display, se siamo in DOPPIO TAGLIO
+      previousMillis = currentMillis;
+      draw(7, u8x8);
+      tone(buzzer, 2000, 200);
+      lapcounter = -1;
+      Serial.println("Case7");  //debug
+      changeState = 0;
+    }
+
+    if (e22ttl.available() > 1) {                      //se il lora riceve qualcosa
+      Serial.println("ricevo qualcosa");               //debug
+      ResponseContainer rc = e22ttl.receiveMessage();  // Receive message
+      State = rc.data;                                 //Assign incoming data on TxData variable
+      Serial.println("Nuovo stato");                   //debug
+      Serial.println(State);                           //debug
+      changeState = 1;                                 //attivo il flag di cambiostato per i display
+      Serial.println("flag");       //debug
+      Serial.println(changeState);  //debug
+    }
+    CurrentPress = millis();
+    if (digitalRead(pulsante) == LOW && (CurrentPress - Lastpress) >= Delaypress && State == Show) {  //se il pulsante è premuto, sono in show e la sicurezza sulle pressioni ripetute è passata
+      Lastpress = millis();                                                                           //memorizzo l'ultima pressione
+      Serial.println("ricevopressione");
+      ResponseStatus rs = e22ttl.sendFixedMessage(Key, Add, Chan, PressShow);  // invio il codice di show
+      tone(buzzer, 2000, 200);
+    }
+    CurrentPress = millis();
+    if (digitalRead(pulsante) == LOW && (CurrentPress - Lastpress) >= Delaypress && State == StartRace && lapcounter < 10) {  //se il pulsante del semaforo è premuto, sono in race e la sicurezza sulle pressioni ripetute è passata
+      Lastpress = millis();
+      lapcounter = lapcounter + 1;
+      Catturatempo(poitem, poiparz, lapcounter, tempo_base);  //balza l'indice a 1Lastpress = millis()
+      float ultimo_tempo = tempo_flt[lapcounter];
+      char bu[10];
+      dtostrf(ultimo_tempo, 4, 3, bu);  //4 is mininum width, 6 is precision
+      String msg = PressTime;
+      msg.concat(",");
+      msg.concat(lapcounter);
+      msg.concat(",");
+      msg.concat(bu);
+      Lastpress = millis();
+      draw(3, u8x8);
+      ResponseStatus rs = e22ttl.sendFixedMessage(Key, Add, Chan, msg);
+      tone(buzzer, 2000, 200);
+      Serial.println("invio");  //debug
+    }
+    CurrentPress = millis();
+
+    if (lapcounter == 10) {  //se il pulsante del semaforo è premuto, sono in race e la sicurezza sulle pressioni ripetute è passata
+      float tot = CalcoloTempo(tempo_flt);
+      tempo_flt[11] = tot;
+      tempo_parziale[11] = (int)(tot * 100);  // per inserire il totale sulla stringa con virgole
+      float ultimo_tempo = tempo_flt[11];
+      char bu[10];
+      dtostrf(ultimo_tempo, 4, 3, bu);  //4 is mininum width, 6 is precision
+      String msg = end10lap;
+      msg.concat(",");
+      msg.concat("11");
+      msg.concat(",");
+      msg.concat(bu);
+      ResponseStatus rs = e22ttl.sendFixedMessage(Key, Add, Chan, msg);
+      State = end10lap;
+      draw(4, u8x8);
+      lapcounter = -1;
+      previousMillis = millis();
+    }
+  }
+}
---
+++ Firmware Sistema/Sistema Pylon Rev2 Message Compleated/TABELLONE/Teensy/serialmethode.h
@@ -0,0 +1,376 @@
+extern boolean newData1;
+extern boolean newData2;
+extern boolean newData3;
+extern boolean newData4;
+extern boolean newData5;
+extern boolean newData6;
+extern boolean newData7;
+extern boolean newData8;
+extern  const byte numChars = 32;
+extern  char receivedChars1[numChars];
+extern char receivedChars2[numChars];
+extern char receivedChars3[numChars];
+extern char receivedChars4[numChars];
+extern char receivedChars5[numChars];
+extern char receivedChars6[numChars];
+extern char receivedChars7[numChars];
+extern char receivedChars8[numChars];
+
+void  recvSerial1() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial1.available() > 0 && newData1 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial1.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars1[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars1[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData1 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial1() {
+  if (newData1 == true) {
+    Serial.println(receivedChars1);
+    newData1 = false;
+  }
+}
+
+
+
+void  recvSerial2() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial2.available() > 0 && newData2 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial2.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars2[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars2[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData2 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial2() {
+  if (newData2 == true) {
+    Serial.println(receivedChars2);
+    newData2 = false;
+  }
+}
+void  recvSerial3() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial3.available() > 0 && newData3 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial3.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars3[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars3[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData3 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial3() {
+  if (newData3 == true) {
+    Serial.println(receivedChars3);
+    newData3 = false;
+  }
+}
+void  recvSerial4() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial4.available() > 0 && newData4 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial4.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars4[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars4[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData4 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial4() {
+  if (newData4 == true) {
+    Serial.println(receivedChars4);
+    newData4 = false;
+  }
+}
+
+
+void  recvSerial5() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial5.available() > 0 && newData5 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial5.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars5[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars5[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData5 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial5() {
+  if (newData5 == true) {
+    Serial.println(receivedChars5);
+    newData5 = false;
+  }
+}
+void  recvSerial6() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial6.available() > 0 && newData6 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial6.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars6[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars6[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData6 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial6() {
+  if (newData6 == true) {
+    Serial.println(receivedChars6);
+    newData6 = false;
+  }
+}
+void  recvSerial7() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial7.available() > 0 && newData7 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial7.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars7[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars7[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData7 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial7() {
+  if (newData7 == true) {
+    Serial.println(receivedChars7);
+    newData7 = false;
+  }
+}
+
+void  recvSerial8() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+
+  while (Serial8.available() > 0 && newData8 == false) {
+    //while (port.available() > 0 ) {
+    rc = Serial8.read();
+
+    if (recvInProgress == true) {
+
+
+      if (rc != endMarker) {
+        receivedChars8[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars8[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData8 = true;
+      }
+
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+
+}
+
+void showSerial8() {
+  if (newData8 == true) {
+    Serial.println(receivedChars8);
+    newData8 = false;
+  }
+}
