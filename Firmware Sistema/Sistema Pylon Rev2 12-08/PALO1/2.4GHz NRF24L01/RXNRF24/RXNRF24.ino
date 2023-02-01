731084c1e76f9a9085e42c3040444fa632cbfc1e
giachy96 <frfgia@libero.it>
2023-02-01 23:48:30 +0100

    prova 2.4

---
+++ Firmware Sistema/Sistema Pylon Rev2 12-08/PALO1/2.4GHz NRF24L01/RXNRF24/RXNRF24.ino
@@ -0,0 +1,65 @@
+/*
+   Arduino nRF24L01 comunicazione base
+
+   Autore  : Andrea Lombardo
+   Web     : http://www.lombardoandrea.com
+   Post    : https://wp.me/p27dYH-Oi
+*/
+//Inclusione delle librerie
+#include <nRF24L01.h>
+#include <RF24_config.h>
+#include <RF24.h>
+
+
+//definizione indirizzo sul quale stabilire la comunicazione
+const byte indirizzo[5] = {0, 0, 0, 0, 0};
+
+//Creo un'istanza della "radio" passandogli il numero dei pin collegati a CE e CSN del modulo
+RF24 radio(5, 4);
+
+
+void setup() {
+
+  //Inizializzo la comunicazione seriale per il monitor
+  Serial.begin(115200);
+
+  //Aspetto ad andare avanti fin quando non viene aperto il monitor seriale
+  while (!Serial) {
+    //Aspetto
+  }
+
+  Serial.println(F("Avvio ricevitore"));
+
+  //Inizializzo la radio
+  radio.begin();
+
+  /*
+     La radio può lavorare a diverse potenze: RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH e RF24_PA_MAX
+     Che corrispondono a: -18dBm, -12dBm,-6dBM, e 0dBm
+  */
+  //Setto la potenza della radio a LOW
+  radio.setPALevel(RF24_PA_LOW);
+
+  //Apro un canale in lettura sull'indirizzo specificato
+  radio.openReadingPipe(1, indirizzo);
+
+  //Metto la radio in ascolto
+  radio.startListening();
+
+  Serial.println(F("Radio in ascolto!"));
+
+}
+
+void loop() {
+
+  //Se ci sono dati in ricezione sulla radio
+  if (radio.available()) {
+    //Creo una variabile di appoggio
+    int buff = 0;
+    //Leggo i dati sul buffer e li salvo nella variabile di appoggio
+    radio.read(&buff, sizeof(buff));
+    //Invio al monitor seriale il valore appena letto
+    Serial.println(buff);
+  }
+
+}
